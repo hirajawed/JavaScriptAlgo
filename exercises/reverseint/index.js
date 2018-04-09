244747142d7221dc9fee3088a1d2ca9793259d85
Hira <hirajawed3@gmail.com>
2018-04-09 15:11:46 -0500

    changed reverseint file

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const myNumber = n;
+  myNumber.toString().split('');
+  return parseInt(myNumber.toString())
+
+}
 
 module.exports = reverseInt;
