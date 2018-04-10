23b10af42148fae3fd1771b47eabc349ccd2ccc9
hirajawed <hirajawed3@gmail.com>
2018-04-10 14:05:37 -0500

    Max Char

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+  let max = 0; //max is the intial variable
+  let maxChar = '';
+
+  for(let char of str){ //FOR / OF Loop is used for iterating through an array or a string.
+    if(charMap[char]){ //if the entry already exists in charMap it should increment by one
+      charMap[char]++
+    }else{ //if the entry does not exists we count it as 1 time.
+      charMap[char] = 1;
+    }
+  }
+  for(let char in charMap){ //WE are iterating thru an actual OBJECT so we use "in" in this method.
+    if(charMap[char] > max){ //if the iterable object is greater than the current max value
+      max = charMap[char];  //update the max value
+      maxChar = char; //also update the character to the new max and return it at the bottom
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+
+//we have iterate through charMap to find the most frequently used element.
